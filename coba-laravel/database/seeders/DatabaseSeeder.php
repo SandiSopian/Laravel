ed01ff75d89a60810c653c38a9cae55921fc5887
Sandi Sopian <shunasand73@gmail.com>
2021-10-01 23:39:50 +0700

    Database seeder

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Sandi',
+            'email' => 'sandi@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        User::create([
+            'name' => 'Sopian',
+            'email' => 'sopian@gmail.com',
+            'password' => bcrypt('123996')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, ',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, minima molestias aliquid itaque iste quidem modi. Natus, numquam dolor voluptatem consequuntur sapiente, nihil culpa minima repudiandae corrupti pariatur rerum quidem. Harum, distinctio excepturi quam dolore facilis id dolorum consequuntur sint expedita, saepe incidunt corporis consectetur quod! Dolore nihil ea quibusdam nemo dignissimos, repellendus in aspernatur non suscipit, pariatur assumenda, aperiam a explicabo eligendi nulla voluptate quo voluptates! Voluptas ut consectetur nostrum obcaecati tenetur iusto! Debitis eum dolorum voluptatum distinctio ex, libero quasi? Quam voluptates saepe ipsum voluptas minus aliquid aspernatur repudiandae reprehenderit provident quae cum, ducimus, fugiat temporibus. Commodi modi voluptatem magnam ab ad pariatur enim hic facilis vel, architecto asperiores atque et?',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, ',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, minima molestias aliquid itaque iste quidem modi. Natus, numquam dolor voluptatem consequuntur sapiente, nihil culpa minima repudiandae corrupti pariatur rerum quidem. Harum, distinctio excepturi quam dolore facilis id dolorum consequuntur sint expedita, saepe incidunt corporis consectetur quod! Dolore nihil ea quibusdam nemo dignissimos, repellendus in aspernatur non suscipit, pariatur assumenda, aperiam a explicabo eligendi nulla voluptate quo voluptates! Voluptas ut consectetur nostrum obcaecati tenetur iusto! Debitis eum dolorum voluptatum distinctio ex, libero quasi? Quam voluptates saepe ipsum voluptas minus aliquid aspernatur repudiandae reprehenderit provident quae cum, ducimus, fugiat temporibus. Commodi modi voluptatem magnam ab ad pariatur enim hic facilis vel, architecto asperiores atque et?',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, ',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, minima molestias aliquid itaque iste quidem modi. Natus, numquam dolor voluptatem consequuntur sapiente, nihil culpa minima repudiandae corrupti pariatur rerum quidem. Harum, distinctio excepturi quam dolore facilis id dolorum consequuntur sint expedita, saepe incidunt corporis consectetur quod! Dolore nihil ea quibusdam nemo dignissimos, repellendus in aspernatur non suscipit, pariatur assumenda, aperiam a explicabo eligendi nulla voluptate quo voluptates! Voluptas ut consectetur nostrum obcaecati tenetur iusto! Debitis eum dolorum voluptatum distinctio ex, libero quasi? Quam voluptates saepe ipsum voluptas minus aliquid aspernatur repudiandae reprehenderit provident quae cum, ducimus, fugiat temporibus. Commodi modi voluptatem magnam ab ad pariatur enim hic facilis vel, architecto asperiores atque et?',
+            'category_id' => 2,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, ',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, debitis veritatis animi rerum quod sint adipisci. Maiores autem impedit voluptate et distinctio pariatur odit ullam aperiam blanditiis voluptatibus, sit atque illo expedita. Accusantium esse odio iure, minima molestias aliquid itaque iste quidem modi. Natus, numquam dolor voluptatem consequuntur sapiente, nihil culpa minima repudiandae corrupti pariatur rerum quidem. Harum, distinctio excepturi quam dolore facilis id dolorum consequuntur sint expedita, saepe incidunt corporis consectetur quod! Dolore nihil ea quibusdam nemo dignissimos, repellendus in aspernatur non suscipit, pariatur assumenda, aperiam a explicabo eligendi nulla voluptate quo voluptates! Voluptas ut consectetur nostrum obcaecati tenetur iusto! Debitis eum dolorum voluptatum distinctio ex, libero quasi? Quam voluptates saepe ipsum voluptas minus aliquid aspernatur repudiandae reprehenderit provident quae cum, ducimus, fugiat temporibus. Commodi modi voluptatem magnam ab ad pariatur enim hic facilis vel, architecto asperiores atque et?',
+            'category_id' => 2,
+            'user_id' => 2,
+        ]);
     }
 }
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. Sandi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none"> {{ $post->user->name }} </a> <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
     
-    <a href="/posts">Back to posts</a>
+    <a href="/posts" class="d-block" mt-4>Back to posts</a>
 @endsection
